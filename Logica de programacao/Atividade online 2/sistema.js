4a326be1d06a3c17bbf5150ab99ebe8c32b6023d
Regisds1402 <regisds1402@gmail.com>
2021-08-27 23:59:51 -0300

    Atividade Encontro Remoto 2 UC-Logica de Programação( Loop For )

--- Logica de programacao/Atividade online 2/sistema.js
+++ Logica de programacao/Atividade online 2/sistema.js
@@ -1,32 +1,33 @@
 let numeroDeAlunos = 10;
 
-let contador = 1;
+let contador = 0;
 
-do {
-    if (contador === 0) {
-        console.log("O numero atual é ZERO")
-    } else if(contador % 2 == 0 && contador > 5) {
-        console.log("O número " + contador + " é PAR")
-    } else {
-        console.log("O número " + contador + " é IMPAR")
-    }
-    contador++
-} while (contador <= numeroDeAlunos);
+// do {
+//     if (contador === 0) {
+//         console.log("O numero atual é ZERO")
+//     } else if(contador % 2 == 0 && contador > 5) {
+//         console.log("O número " + contador + " é PAR")
+//     } else {
+//         console.log("O número " + contador + " é IMPAR")
+//     }
+//     contador++
+// } while (contador <= numeroDeAlunos);
 
-/*while (contador <= numeroDeAlunos) {
-    if (contador === 0) {
-        console.log("O numero atual é ZERO")
-    } else if(contador % 2 != 0) {
-        console.log("O número " + contador + " é PAR")
-    } else {
-        console.log("O número " + contador + " é IMPAR")
-    }
+
+// while (contador1 <= qtdAlunos) {
+//     if (contador1 === 0) {
+//         console.log("O numero atual é ZERO")
+//     } else if(contador1 % 2 != 0) {
+//         console.log("O número " + contador1 + " é PAR")
+//     } else {
+//         console.log("O número " + contador1 + " é IMPAR")
+//     }
     
-    contador++;
-}
+//     contador++;
+// }
 
 for (let contador = 0; contador <= numeroDeAlunos; contador ++) {
-    console.log(contador);
+    // console.log(contador);
 
     if (contador === 0) {
         console.log("O numero atual é ZERO");
@@ -39,12 +40,12 @@ for (let contador = 0; contador <= numeroDeAlunos; contador ++) {
 
 
 
-let listaDeAlunos = ["Marcelo", "Juh", "Cleber", "Wesley", "Adriano"]
+// let listaDeAlunos = ["Marcelo", "Juh", "Cleber", "Wesley", "Adriano"]
 
-for (const aluno of listaDeAlunos) {
-    console.log(aluno);
-}
+// for (const aluno of listaDeAlunos) {
+//     console.log(aluno);
+// }
 
-listaDeAlunos.forEach(aluno => {
-    console.log(aluno)
-});*/
+// listaDeAlunos.forEach(aluno => {
+//     console.log(aluno)
+// }
